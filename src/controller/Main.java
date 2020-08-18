517252a6c0d20c08b8aa7528abd618a92410f7c3
charanjarwal <charanjarwal@LAPTOP-O72MBBSG.localdomain>
2020-08-18 10:48:10 +0530

    charan

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -8,6 +8,18 @@ public class Main {
 	public static void main(String args[]) {
 		
 	// Fill your code
-		
+		Connection con  = null;
+		try {
+			con = ConnectionManager.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(con == null) {
+			System.out.println("Check your connection");
+		}
+		else {
+			System.out.println("Connection established");
+		}
 	}
 }
